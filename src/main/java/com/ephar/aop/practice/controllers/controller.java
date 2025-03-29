70326f19770456ff0e0945f4f68417b74e285016
Kraphe <kraphe001@gmail.com>
2025-03-29 14:48:38 +0530

    first commit

---
+++ src/main/java/com/ephar/aop/practice/controllers/controller.java
@@ -0,0 +1,15 @@
+package com.ephar.aop.practice.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class controller {
+    @GetMapping("is")
+    public void get(){
+        throw new RuntimeException();
+    }
+}
