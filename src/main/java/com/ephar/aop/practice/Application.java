fa030935a65cc4a0645c77504883c6655491328f
Kraphe <98689504+Kraphe@users.noreply.github.com>
2025-04-06 16:31:39 +0530

    Update Application.java

--- src/main/java/com/ephar/aop/practice/Application.java
+++ src/main/java/com/ephar/aop/practice/Application.java
@@ -6,11 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	String API="Mafaka";  //Comment Added at line 9
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
 	
 	public void functionoffunction(){
 		System.print.out("hello");
